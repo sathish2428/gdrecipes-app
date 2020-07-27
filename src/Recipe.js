6de914848c53505d525193a18cd70ffbbb2235b5
sathish kumar <sathishds2428@gmail.com>
2020-07-27 14:22:54 +0530

    2nd commit

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,19 +2,7 @@ import React from "react";
 
 export const Recipe = (props) => {
     return (
-    //     <div className="card rounded box-shadow">
-    //         <img src={props.image} className="card-img-top" alt={props.title} />
-    //         <div className="card-body">
-    //             <h5 className="card-title">{props.title}</h5>
-    //             <h4>Ingredients: </h4>
-    //             {props.ingredients.map((ingredient, i) => {
-    //                 return (<p className="card-text" key={i}>{i + 1}. {ingredient} </p>)
-    //             })}
-    //             <p className="card-text"><small className="text-muted"></small></p>
-    //         </div>
-    //     </div>
-    // )
-    
+       
         <div className="board">
             <div style={{backgroundImage:"url("+props.image+")"}} className="banner"></div>
             
